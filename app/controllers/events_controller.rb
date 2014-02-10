f13e1845e82628ecc58a06b987b8b2bc81b07357
Cevaris <cevaris@gmail.com>
2014-02-09 21:54:50 -0700

    blah

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,12 +2,7 @@ class EventsController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: [:create]
 
   def create
-    @producer = Kafka::Producer.new(host: 'localhost', port: '9092', topic: "events")
-    # event_handler.fire(params[:event])
-    event = params[:event]
-    
-    Rails.logger.info "Pushing event #{event}"
-    @producer.push(Kafka::Message.new(event))
+    event_handler.fire(params[:event])
     render status: 200, nothing: true
   end
 end
--- lib/kafka_events.rb
+++ lib/kafka_events.rb
@@ -8,21 +8,20 @@ class KafkaEvents
   end
 
   def write(messages)
-    puts 'Writing messages to queue'
     @queue.push(messages)
   end
 
   def start(producer)
     Thread.new do
-      while true
-        batch = @queue.pop
+      while batch = @queue.pop
         producer.batch do
           batch.each do |message|
-            Rails.logger.info "PreKafka - #{message}"
+            puts "PreKafka - #{message}"
             producer.send(Kafka::Message.new(message))
           end
         end
       end
     end
+    
   end
 end
