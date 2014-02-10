157f17e5f7161e7eeb039b1a68ffc1ceddcd3896
Cevaris <cevaris@gmail.com>
2014-02-09 19:35:53 -0700

    just going to do twitter streaming

---
+++ scripts/events/client.rb
@@ -0,0 +1,14 @@
+include 'mobile'
+
+class Client
+  clients = ['296b9b30-9eb0-46f6-92d9-d2a64c3c424d', '45b67644-3900-4fe9-912b-0e388db9eeb9',
+             'd38a26d0-6a84-4984-9246-8955783d2413', 'e1ea6d7c-abe9-40f0-bae7-b8359d57d1ca', 
+             '2a7e6540-da44-4fbf-b799-819e41038ae6', '789a3828-6905-4d10-afd9-0b0b92bf3bb1', 
+             'ec3240dd-997b-4184-9d5b-d8ce32a722bb', '5e5cde58-cb4c-41c4-8826-807765ac2022',
+             'a3029374-249d-43c2-b572-23ca11d3de02', 'b39b3fcf-d738-4ac5-9eb5-f4487feee4bb'}
+
+  
+
+
+
+end
